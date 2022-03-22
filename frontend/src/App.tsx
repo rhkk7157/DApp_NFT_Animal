70a2ae30750f6f4c40b0c6dc70320384a93d93bd
hyeri <rhkk7157@gmail.com>
2022-03-22 18:27:22 +0900

    프론트엔드 셋팅 (chakraUI, React-Router-Dom)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,16 @@
-import React, { FC } from "react";
+import React, { FC } from 'react';
+import { Button } from '@chakra-ui/react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Main from './routes/Main';
 
 const App: FC = () => {
-  return <div>web3-boilerplate</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Main />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
